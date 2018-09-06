656e86c2946abee05ce3e48e79889295b5a8cc1d
Iván López <lopez.ivan@gmail.com>
2018-09-06 13:42:59 +0200

    Add Book and Genre domains

---
+++ complete/src/main/java/example/micronaut/domain/Book.java
@@ -0,0 +1,67 @@
+package example.micronaut.domain;
+
+import javax.validation.constraints.NotNull;
+
+public class Book {
+
+    public Book() {
+    }
+
+    public Book(@NotNull String isbn, @NotNull String name, Genre genre) {
+        this.isbn = isbn;
+        this.name = name;
+        this.genre = genre;
+    }
+
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String isbn;
+
+    private Genre genre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", genre=" + genre +
+                '}';
+    }
+}
---
+++ complete/src/main/java/example/micronaut/domain/Genre.java
@@ -0,0 +1,57 @@
+package example.micronaut.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Genre {
+
+    public Genre() {
+    }
+
+    public Genre(@NotNull String name) {
+        this.name = name;
+    }
+
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @JsonIgnore
+    private Set<Book> books = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Genre{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
