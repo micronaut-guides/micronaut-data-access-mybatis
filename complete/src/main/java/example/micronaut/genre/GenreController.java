4ecc9f3d18b09fad1ee2e5127df746128febe9b2
Iván López <lopez.ivan@gmail.com>
2018-09-07 12:33:49 +0200

    Add GenreController, command objects and tests

---
+++ complete/src/main/java/example/micronaut/genre/GenreController.java
@@ -0,0 +1,86 @@
+package example.micronaut.genre;
+
+import example.micronaut.ApplicationConfiguration;
+import example.micronaut.PaginationArguments;
+import example.micronaut.SortingArguments;
+import example.micronaut.SortingOrder;
+import example.micronaut.domain.Genre;
+import io.micronaut.http.HttpHeaders;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Delete;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Put;
+import io.micronaut.validation.Validated;
+
+import javax.annotation.Nullable;
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@Validated // <1>
+@Controller("/genres") // <2>
+public class GenreController {
+
+    protected final GenreRepository genreRepository;
+
+    protected final ApplicationConfiguration applicationConfiguration;
+
+    public GenreController(GenreRepository genreRepository,
+                           ApplicationConfiguration applicationConfiguration) { // <3>
+        this.genreRepository = genreRepository;
+        this.applicationConfiguration = applicationConfiguration;
+    }
+
+    @Get("/{id}") // <4>
+    public Genre show(Long id) {
+        return genreRepository.findById(id); // <5>
+    }
+
+    @Put("/") // <6>
+    public HttpResponse update(@Body @Valid GenreUpdateCommand command) { // <7>
+        genreRepository.update(command.getId(), command.getName());
+
+        return HttpResponse
+                .noContent()
+                .header(HttpHeaders.LOCATION, location(command.getId()).getPath()); // <8>
+    }
+
+    @Get(value = "/") // <9>
+    public List<Genre> list(@Nullable Integer offset,
+                            @Nullable Integer max,
+                            @Nullable String sort,
+                            @Nullable String order) {
+        PaginationArguments paginationArguments = new PaginationArguments(offset != null ? offset : 0,
+                max != null ? max : applicationConfiguration.getMax());
+        SortingArguments sortingArguments = sort != null ? new SortingArguments(sort, SortingOrder.of(order)) : null;
+
+        return genreRepository.findAll(paginationArguments, sortingArguments);
+    }
+
+    @Post("/") // <10>
+    public HttpResponse<Genre> save(@Body @Valid GenreSaveCommand cmd) {
+        Genre genre = new Genre(cmd.getName());
+        genreRepository.save(genre);
+
+        return HttpResponse
+                .created(genre)
+                .headers(headers -> headers.location(location(genre.getId())));
+    }
+
+    @Delete("/{id}") // <11>
+    public HttpResponse delete(Long id) {
+        genreRepository.deleteById(id);
+        return HttpResponse.noContent();
+    }
+
+    protected URI location(Long id) {
+        return URI.create("/genres/" + id);
+    }
+
+    protected URI location(Genre genre) {
+        return location(genre.getId());
+    }
+}
---
+++ complete/src/test/java/example/micronaut/genre/GenreControllerTest.java
@@ -0,0 +1,120 @@
+package example.micronaut.genre;
+
+import static org.junit.Assert.assertEquals;
+
+import example.micronaut.domain.Genre;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.core.type.Argument;
+import io.micronaut.http.HttpHeaders;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.runtime.server.EmbeddedServer;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenreControllerTest {
+
+    private static EmbeddedServer server; // <1>
+    private static HttpClient client; // <2>
+
+    @BeforeClass
+    public static void setupServer() {
+        server = ApplicationContext
+                .build()
+                .packages("example.micronaut.domain") // <3>
+                .run(EmbeddedServer.class);
+        client = server.getApplicationContext().createBean(HttpClient.class, server.getURL()); // <2>
+    }
+
+    @AfterClass
+    public static void stopServer() {
+        if (server != null) {
+            server.stop();
+        }
+        if (client != null) {
+            client.stop();
+        }
+    }
+
+    @Test
+    public void testGenreCrudOperations() {
+
+        List<Long> genreIds = new ArrayList<>();
+
+        HttpRequest request = HttpRequest.POST("/genres", new GenreSaveCommand("DevOps")); // <4>
+        HttpResponse response = client.toBlocking().exchange(request);
+        genreIds.add(entityId(response));
+
+        assertEquals(HttpStatus.CREATED, response.getStatus());
+
+        request = HttpRequest.POST("/genres", new GenreSaveCommand("Microservices")); // <5>
+        response = client.toBlocking().exchange(request);
+
+        assertEquals(HttpStatus.CREATED, response.getStatus());
+
+        Long id = entityId(response);
+        genreIds.add(id);
+        request = HttpRequest.GET("/genres/"+id);
+
+        Genre genre = client.toBlocking().retrieve(request, Genre.class); // <4>
+
+        assertEquals("Microservices", genre.getName());
+
+        request = HttpRequest.PUT("/genres", new GenreUpdateCommand(id, "Micro-services"));
+        response = client.toBlocking().exchange(request);  // <6>
+
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatus());
+
+        request = HttpRequest.GET("/genres/" + id);
+        genre = client.toBlocking().retrieve(request, Genre.class);
+        assertEquals("Micro-services", genre.getName());
+
+        request = HttpRequest.GET("/genres");
+        List<Genre> genres = client.toBlocking().retrieve(request, Argument.of(List.class, Genre.class));
+
+        assertEquals(2, genres.size());
+
+        request = HttpRequest.GET("/genres?max=1");
+        genres = client.toBlocking().retrieve(request, Argument.of(List.class, Genre.class));
+
+        assertEquals(1, genres.size());
+        assertEquals("DevOps", genres.get(0).getName());
+
+        request = HttpRequest.GET("/genres?max=1&order=desc&sort=name");
+        genres = client.toBlocking().retrieve(request, Argument.of(List.class, Genre.class));
+
+        assertEquals(1, genres.size());
+        assertEquals("Micro-services", genres.get(0).getName());
+
+        request = HttpRequest.GET("/genres?max=1&offset=10");
+        genres = client.toBlocking().retrieve(request, Argument.of(List.class, Genre.class));
+
+        assertEquals(0, genres.size());
+
+        // cleanup:
+        for (Long genreId : genreIds) {
+            request = HttpRequest.DELETE("/genres/"+genreId);
+            response = client.toBlocking().exchange(request);
+            assertEquals(HttpStatus.NO_CONTENT, response.getStatus());
+        }
+    }
+
+    protected Long entityId(HttpResponse response) {
+        String path = "/genres/";
+        String value = response.header(HttpHeaders.LOCATION);
+        if ( value == null) {
+            return null;
+        }
+        int index = value.indexOf(path);
+        if ( index != -1) {
+            return Long.valueOf(value.substring(index + path.length()));
+        }
+        return null;
+    }
+}
