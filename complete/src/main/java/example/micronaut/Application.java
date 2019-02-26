baa34d96fb573baa5a972a0ce4595d264c069bfe
Iván López <lopez.ivan@gmail.com>
2019-02-26 15:29:50 +0100

    Add Flyway to manage database schema

--- complete/src/main/java/example/micronaut/Application.java
+++ complete/src/main/java/example/micronaut/Application.java
@@ -1,44 +1,11 @@
 package example.micronaut;
 
-import io.micronaut.context.event.StartupEvent;
 import io.micronaut.runtime.Micronaut;
-import io.micronaut.runtime.event.annotation.EventListener;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.jdbc.ScriptRunner;
-
-import javax.sql.DataSource;
-import java.io.IOException;
-import java.io.Reader;
-import java.sql.Connection;
-import java.sql.SQLException;
 
 public class Application {
-
-    private DataSource dataSource;
-
-    public Application(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
 
     public static void main(String[] args) {
         Micronaut.run(Application.class);
     }
 
-    @EventListener
-    public void onStartup(StartupEvent event) {
-        try {
-            Connection conn = dataSource.getConnection();
-            Reader reader = Resources.getResourceAsReader("schema.sql");
-
-            ScriptRunner runner = new ScriptRunner(conn);
-            runner.runScript(reader);
-            conn.commit();
-            reader.close();
-        } catch (IOException e) {
-            // schema.sql doesn't exit. Skip execution.
-        } catch (SQLException e) {
-            // Ignore. If there is a problem getting DB connection the application doesn't start and this listener
-            // is never executed
-        }
-    }
 }
